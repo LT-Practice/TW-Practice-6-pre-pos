e06622f0c26dcbf60845cd4ff864cbb3008311d6
Leonard LiuTao <364125168@qq.com>
2016-07-14 17:28:03 +0800

    section_3_4

--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,35 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  var c = [];
+
+  for (var i = 0; i < collection_a.length; i++) {
+    var exist = false;
+    var char = collection_a[i].length > 2 ? collection_a[i].substring(0, 1) : collection_a[i];
+    var count = collection_a[i].length > 2 ? count = collection_a[i].substring(2) : 1;
+
+    for (var j = 0; j < c.length; j++) {
+      if (c[j].key == char) {
+        c[j].count += count;
+        exist = true;
+      }
+    }
+
+    if (!exist) {
+      var sum = {};
+      sum.key = char;
+      sum.count = count;
+      c.push(sum);
+    }
+  }
+
+  for (var i = 0; i < c.length; i++) {
+    for (var j = 0; j < object_b.value.length; j++) {
+      if (c[i].key == object_b.value[j]) {
+        c[i].count -= parseInt(c[i].count / 3);
+      }
+    }
+  }
+
+  return c;
 }
 
 module.exports = create_updated_collection;
